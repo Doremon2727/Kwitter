230dba9c0302c41cb8e804f3c3a027e5251adaaa
Doremon2727 <87557272+Doremon2727@users.noreply.github.com>
2021-07-20 17:22:26 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB98l2UBnsTg6XJej6WcF-1ikdo-uspNuA",
+    authDomain: "project-92-ef32f.firebaseapp.com",
+    databaseURL: "https://project-92-ef32f-default-rtdb.firebaseio.com",
+    projectId: "project-92-ef32f",
+    storageBucket: "project-92-ef32f.appspot.com",
+    messagingSenderId: "393027262242",
+    appId: "1:393027262242:web:6c6d1891670dd24fb4f77b"
+  };
+ 
+  firebase.initializeApp(firebaseConfig);
+
+  firebase.initializeApp(firebaseConfig);
+
+    
